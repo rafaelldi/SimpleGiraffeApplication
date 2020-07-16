7b4174a7da5819aedce57e7c50835116c9b7b97f
rafaelldi <rival.abdrakhmanov@outlook.com>
2020-07-16 21:36:28 +0300

    Add giraffe

--- Program.fs
+++ Program.fs
@@ -1,27 +1,25 @@
 namespace SimpleGiraffeApplication
 
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
+open Giraffe
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 
 module Program =
-    let exitCode = 0
-
-    let CreateHostBuilder args =
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>() |> ignore
-            )
+    let webApp =
+        choose [
+            route "/" >=> text "Hello World!"
+            ]
 
     [<EntryPoint>]
     let main args =
-        CreateHostBuilder(args).Build().Run()
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(
+                fun webBuilder ->
+                    webBuilder
+                        .ConfigureServices(fun services -> services.AddGiraffe() |> ignore)
+                        .Configure(fun builder -> builder.UseGiraffe webApp)
+                        |> ignore)
+            .Build()
+            .Run()
 
-        exitCode
+        0
